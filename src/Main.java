821113d69516ff0c5db085d3e1a8164d6f6cc72a
gibrandarmawan <88297926+gibrandarmawan@users.noreply.github.com>
2021-10-01 13:32:06 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ package src;
 
 import java.util.Scanner;
 
-import jdk.internal.jshell.tool.resources.l10n;
+
 
 import java.util.*;
 
 
 public class Main {
-<<<<<<< HEAD
-    public static void main (String[] args) {
-=======
+
     public static void main(String[] args) {
         outputCapturer capture = new outputCapturer();
->>>>>>> 59cb9a867f42d5a7df05c80be9c73e8c42a86aa1
+        Load ambil =  new Load();
+        String captured;
         int choice = 0;
         while (choice != 6) {
         System.out.println("MENU");        
@@ -26,39 +25,22 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         choice = scan.nextInt();
         if (choice == 1) {
-<<<<<<< HEAD
-        	System.out.println("1. Metode eliminasi Gauss");
-        	System.out.println("2. Metode eliminasi Gauss-Jordan");
-        	System.out.println("3. Metode matriks balikan");
-        	System.out.println("4. Metode kaidah Cramer");
-            int x = scan.nextInt();
-            if (x == 1) {
-                System.out.println("Sistem masih dalam pengembangan");      
-            } 
-            else if ( x == 2) {
-                System.out.println("Sistem masih dalam pengembangan");
-            }
-            else if (x == 3) {
-                System.out.println("Sistem masih dalam pengembangan");
-            }
-            else if (x == 4) {
-                System.out.println("Sistem masih dalam pengembangan");
-            } 
-            else {
-                System.out.println("Input tidak valid!");
-            }
-            
-=======
             int baris, kolom;
-            matriks m;
+            matriksParametrik m;
             System.out.println("1. Metode eliminasi Gauss");
             System.out.println("2. Metode eliminasi Gauss-Jordan");
             System.out.println("3. Metode matriks balikan");
             System.out.println("4. Kaidah Cramer");
             int splPilih = scan.nextInt();
+            
             if (splPilih == 1){
-                if(/*external.loadStatus*/baris == 1){
-                    // M=external.bacaMatrixFile("../test/data_input/file_externalmatrix.txt"); 
+                ambil.cekSimpan();
+                if(ambil.simpanStatus()){
+                    ambil.namaExternal();
+                }
+                ambil.cekLoad();
+                if(ambil.loadStatus()){
+                    m = ambil.bacaMatrixFile("../test/inputFile.txt"); 
                 }else{
                     System.out.print("Banyak persamaan : ");
                     baris = scan.nextInt();
@@ -66,23 +48,31 @@ public class Main {
                     kolom = scan.nextInt();
                     System.out.println("Masukkan persamaan : ");
                     m = new matriksParametrik(baris, kolom + 1);
-                    m.readMatrix();
-                    capture.start();
-                    m.displayMatrix();
+                }
+                m.readMatrix();
+                capture.start();
+                m.displayMatrix();
+                System.out.println("Memulai algoritma");
                     try{
                         m.rowEsMatrix();
                         m.displayMatrix();
                         System.out.println("^ Matrix hasil eliminasi Gauss ^");
-                        .genStatus();
+                        m.genStatus();
                         m.parametrikGauss();
                     } catch(noSolution n){
                         System.out.println("Tidak ada solusi");
                         }
-                    }
-                }
+            captured = capture.stop(); 
+            }
+                
             else if(splPilih == 2){
-                if(/*external.loadStatus*/baris == 1){
-                    // M=external.bacaMatrixFile("../test/data_input/file_externalmatrix.txt"); 
+                ambil.cekSimpan();
+                if(ambil.simpanStatus()){
+                    ambil.namaExternal();
+                }
+                ambil.cekLoad();
+                if(ambil.loadStatus()){
+                     m = ambil.bacaMatrixFile("../test/inputFile.txt"); 
                 }else{
                     System.out.print("Banyak persamaan : ");
                     baris = scan.nextInt();
@@ -90,9 +80,10 @@ public class Main {
                     kolom = scan.nextInt();
                     System.out.println("Masukkan persamaan : ");
                     m = new matriksParametrik(baris, kolom + 1);
-                    m.readMatrix();
-                    capture.start();
-                    m.displayMatrix();
+                }
+                m.readMatrix();
+                capture.start();
+                m.displayMatrix();
                     try{
                         m.reducedRowMatrix();
                         m.displayMatrix();
@@ -102,7 +93,7 @@ public class Main {
                     } catch(noSolution n){
                         System.out.println("Tidak ada solusi");
                         }
-
+            captured = capture.stop();
             }
             else if(splPilih == 3){
 
@@ -110,7 +101,7 @@ public class Main {
             else if(splPilih == 4){
 
             }
->>>>>>> 59cb9a867f42d5a7df05c80be9c73e8c42a86aa1
+
         }
         else if (choice == 2) {
             System.out.println("1. Metode Eliminasi Gauss");
@@ -123,7 +114,7 @@ public class Main {
                 matriks m = new matriks(N, N);
                 m.readMatrix();
                 System.out.println("Matriks eselon barisnya adalah : ");
-                m.rowEsMatrix2();
+                m.rowEsMatrix();
                 m.displayMatrix();
                 System.out.println("Determinannya adalah : " );
                 System.out.print(matriks.determinan_gauss(m) + "\n");
@@ -142,6 +133,8 @@ public class Main {
         else if (choice == 3) {
             System.out.println("Sistem masih dalam pengembangan");            
         }
+
+
         else if (choice == 4) {
             System.out.println("Masukkan jumlah titik yang diinginkan : ");
             int N = scan.nextInt();
@@ -166,5 +159,5 @@ public class Main {
 
 
     }
-}
+} 
 
